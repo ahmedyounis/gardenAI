d5f2bbc7c72e48bd487b2eff0c7b6d8621f09686
ahmed <ahmed.m.younis@gmail.com>
2020-03-21 22:57:21 -0400

    initialize files

---
+++ env.js
@@ -0,0 +1,39 @@
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var pump = new Gpio(23, 'out'); //use GPIO pin 4, and specify that it is output
+var pumpRunning = false;
+var sleep = require('sleep');
+
+if (pump.readSync() === 0) {
+} else {
+    pump.writeSync(0);
+}
+
+
+function runPump() {
+    console.log('Running Pump');
+    pump.writeSync(1);
+}
+
+function endPump() { //function to stop blinking
+    console.log('Stoping Pump');
+    pump.writeSync(0); // Turn LED off
+    pump.unexport(); // Unexport GPIO to free resources
+}
+
+process.on('exit', function (err) {
+    endPump();
+});
+
+process.on('uncaughtException', function (err) {
+    console.log('Shit! bug->' + err);
+    endPump();
+});
+
+
+while (pumpRunning = true) {
+    runPump();
+    console.log('sleep for 60 sec');
+    sleep.sleep(5);
+    endPump();
+
+}
