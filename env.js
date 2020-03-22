53d020650990ecf69ecdd464b72ed28b7e299b43
ahmed <ahmed.m.younis@gmail.com>
2020-03-21 23:18:42 -0400

    another change

--- env.js
+++ env.js
@@ -1,19 +1,36 @@
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var pump = new Gpio(23, 'out'); //use GPIO pin 4, and specify that it is output
+var pump = new Gpio(pumpGPIO, 'out'); //use GPIO pin 4, and specify that it is output
 var pumpRunning = false;
 var sleep = require('sleep');
 
+let pumpGPIO = 23;
+let waterTime = 120;    // TIME TO WATER THE PLANTS IN SECCONDS
 
-// make sure the pump is off
+
+// Initializations
 if (pump.readSync() === 0) {
 } else {
     pump.writeSync(0);
 }
 
 
+// Main program 
+while (pumpRunning = true) {
+    runPump();
+    console.log('sleeping for 1 hour');
+    sleep.sleep(3600)
+}
+
+
+
+// Functions 
+
 function runPump() {
-    console.log('Running Pump');
-    pump.writeSync(1);
+    console.log('Running Water Pump');
+    pump.writeSync(1); 
+    sleep.sleep(waterTime);  
+    pump.writeSync(0); 
+    console.log('Stopping Water Pump');
 }
 
 function endPump() { //function to stop blinking
@@ -30,12 +47,3 @@ process.on('uncaughtException', function (err) {
     console.log('Shit! bug->' + err);
     endPump();
 });
-
-
-while (pumpRunning = true) {
-    runPump();
-    console.log('sleep for 60 sec');
-    sleep.sleep(5);
-    endPump();
-
-}
