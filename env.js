5ad14b3bab2b0bb41bc2ec3fa98efa47444d9888
ahmed <ahmed.m.younis@gmail.com>
2020-07-19 02:32:17 -0400

    added npm packages

--- env.js
+++ env.js
@@ -3,8 +3,8 @@ let pumpGPIO = 23;
 let waterTime = 60;        // TIME TO WATER THE PLANTS IN SECCONDS
 let waterBreakTime = 3600;  // TIME TO WAIT IN BETWEEN WATER CYCLES in seconds
 
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var pump = new Gpio(pumpGPIO, 'out'); //use GPIO pin 4, and specify that it is output
 var pumpRunning = false;
 
@@ -14,8 +14,6 @@ const cliProgress = require('cli-progress');
 const { exec } = require("child_process");
 var sleep = require('sleep');
 var CronJob = require('cron').CronJob;
-
-
 
 // INITIALIZATION 
 if (pump.readSync() === 0) {
